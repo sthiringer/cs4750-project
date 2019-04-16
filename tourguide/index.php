cf5ab079db7ecb3834be224e7e33fcf5acd67e5a
devashisdas0 <devashisdas0@gmail.com>
2019-04-16 17:39:11 -0400

    Fixed html on landing page to require data in forms, and also fixed html look and feel for adding a tour page

--- tourguide/index.php
+++ tourguide/index.php
@@ -1,18 +1,34 @@
 <html>
 <head>
+  <meta charset="utf-8">
+
+  <title>Add Tour Page</title>
+
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
 </head>
 <body>
-<h1>Welcome to the tour guide admin page</h1>
-<h3>Available actions for tour guides are listed here.</h3>
+<div class="container text-center pt-3">
+  <h3>Enter information for a tour here!</h3>
+</div>
 
-  <h5>Enter information for a tour here!</h5>
-  <form class="form-group" action="addTour.php" method="post">
-    <p>Tour Name: </p><input style="max-width:25%" class="form-control" type="text" name="tour_name">
-    <p>Date: </p><input style="max-width:15%" class="form-control input-sm" type="date" name="date">
-    <p>Start Time: </p><input style="max-width:10%" class="form-control input-sm" type="time" name="start">
-    <p>End Time: </p><input style="max-width:10%" class="form-control input-sm" type="time" name="end">
-    <input class="btn btn-primary" type="submit">
+ <div class="container">
+  <form action="addTour.php" method="post">
+    <div class="form-group">
+      <label for="tourname">Tour Name: </label><input class="form-control" type="text" name="tourname" id="tourname" required>
+      </div>
+    <div class="form-group">
+      <label for="date">Date: </label><input class="form-control" type="date" name="date" id="date" required>
+      </div>
+    <div class="form-group">
+      <label for="start">Start Time: </label><input class="form-control" type="time" name="start" id="start" required>
+      </div>
+    <div class="form-group">
+      <label for="end">End Time: </label><input class="form-control" type="time" name="end" id="end" required>
+      </div>
+    <div class="container text-center p-3">
+      <input class="btn btn-primary" type="submit">
+      </div>
   </form>
 
 </body>
